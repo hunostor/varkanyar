dc96438b49ea1cc666c3a1843a2035fdab7ba114
Poroszkai Attila <poroszkai.attila@gmail.com>
2016-08-11 21:52:01 +0200

    Vegas slideshow setup
    vegas/setup.js file edited

--- vegas/setup.js
+++ vegas/setup.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	$('body').vegas({
     delay: 19000,
     transition: 'random',
-    transitionDuration: 9000,
+    transitionDuration: 15000,
     slides: [
         { src: 'kepek/1.jpg' },
         { src: 'kepek/2.jpg' },
@@ -10,9 +10,12 @@ $(document).ready(function () {
         { src: 'kepek/4.jpg' },
         { src: 'kepek/5.jpg' },
         { src: 'kepek/6.jpg' },
-        { src: 'kepek/7.jpg' }
+        { src: 'kepek/7.jpg' },
+        { src: 'kepek/8.jpg' },
+        { src: 'kepek/9.jpg' }
        ],
        overlay: "overlays/03.png",
-       transition: [ 'swirlLeft2', 'zoomOut','swirlRight2', 'swirlLeft','swirlRight' ]
+       transition: [ 'swirlLeft2', 'zoomOut','swirlRight2', 'swirlLeft','swirlRight', 'zoomIn', 'zoomOut2' ],
+       animation: 'random'
       }); 
 });
