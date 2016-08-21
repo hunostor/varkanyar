7576ed43d3b55e92114a6dda457cd629c5249757
Poroszkai Attila <poroszkai.attila@gmail.com>
2016-08-21 22:54:40 +0200

    Responsive setup vegas backgorund image script
    Mobilon a bongeszes gyorsitasa miatt a vegas script korlatozasra kerult
    600 pixel feletti bongeszobe valo betoltodes eseten kapcsol csak be

--- vegas/setup.js
+++ vegas/setup.js
@@ -1,23 +1,31 @@
 $(document).ready(function () {
-	$('body').vegas({
-    delay: 15000,
-    transition: 'fade',
-    transitionDuration: 2000,
-    slides: [
-        { src: 'kepek/banner/1.jpg', delay: '5000', transition: 'fade' },
-        { src: 'kepek/banner/2.jpg' },
-        { src: 'kepek/banner/3.jpg' },
-        { src: 'kepek/banner/4.jpg' },
-        { src: 'kepek/banner/5.jpg' },
-        { src: 'kepek/banner/6.jpg' },
-        { src: 'kepek/banner/7.jpg' },
-        { src: 'kepek/banner/8.jpg' },
-        { src: 'kepek/banner/9.jpg' },
-		{ src: 'kepek/banner/10.jpg' },
-        { src: 'kepek/banner/11.jpg' }
-       ],
-       
-      
-       animation: 'random'
-      }); 
+    // var w == a bongeszo szelessege pixelben
+    var w = window.innerWidth;
+    if (w > 600) { // 600 pixel felett ez a blokk hajtodik vegre
+    	$('body').vegas({
+        delay: 15000,
+        transition: 'fade',
+        transitionDuration: 2000,
+        slides: [
+            { src: 'kepek/banner/1.jpg', delay: '5000', transition: 'fade' },
+            { src: 'kepek/banner/2.jpg' },
+            { src: 'kepek/banner/3.jpg' },
+            { src: 'kepek/banner/4.jpg' },
+            { src: 'kepek/banner/5.jpg' },
+            { src: 'kepek/banner/6.jpg' },
+            { src: 'kepek/banner/7.jpg' },
+            { src: 'kepek/banner/8.jpg' },
+            { src: 'kepek/banner/9.jpg' },
+    		{ src: 'kepek/banner/10.jpg' },
+            { src: 'kepek/banner/11.jpg' }
+           ],
+           animation: 'random'
+          });
+    } else {
+        $('body').vegas({
+        slides: [
+            { src: 'kepek/banner/1.jpg',},
+           ],
+          });
+    }
 });
