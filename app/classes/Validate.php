b25061f08820623d9970f71bef3b147bf5d4201d
Poroszkai Attila <poroszkai.attila@gmail.com>
2016-08-23 20:31:11 +0200

    add comment a validali classhoz

--- app/classes/Validate.php
+++ app/classes/Validate.php
@@ -1,7 +1,11 @@
 <?php 
 
+/**
+ * Automatikusan validalja a form felol erkezeo array -be gyujtott adatokat
+ */
 class Validate
 {
+	// a valid adatokkal visszatero array
 	protected $validData;
 
 	public function __construct($dirtyDataArray)
@@ -17,11 +21,22 @@ class Validate
 		return $this->validData;
 	}
 
+	/**
+	 * kiszedi a html tagokat a stringekbol
+	 * @param  string $dirtyData html tagokat is tartalmazhato string
+	 * @return string            html tagoktol mentes string
+	 */
 	protected function removeHtmlTag($dirtyData)
 	{
 		return strip_tags($dirtyData);
 	}
 
+	/**
+	 * ellenorzi valoban szabalyos email cimet adtak-e meg
+	 * @param  string $email barmi lehet
+	 * @return boolen        true ha valid az emailcim
+	 *                       false ha nem ervenyes
+	 */
 	protected function validateEmail($email)
 	{
 		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -32,6 +47,11 @@ class Validate
 		}
 	}
 
+	/**
+	 * torli a string elejerol es a vegerol a szokozoket
+	 * @param  string $dirtyData valamilyen sring
+	 * @return string            az elejen es a vegen nincsennek szokozok
+	 */
 	protected function removeWhiteSpace($dirtyData)
 	{
 		return trim($dirtyData);
