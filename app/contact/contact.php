3d9e23a552a220ca039a6861d5d5f84b344ba57a
Poroszkai Attila <poroszkai.attila@gmail.com>
2016-08-31 14:58:33 +0200

    cookie create

--- app/contact/contact.php
+++ app/contact/contact.php
@@ -15,8 +15,16 @@ $dirtyDataArray = [
 try {
 	// Adatok validalasa
 	$validData = new Validate($dirtyDataArray);
+	
 	// Email elkuldese az uzenettel
 	$send = new SendMail($validData->getValidData());
+
+	// ha elkuldte az emailt, akkor beallitja a cookiet
+	if ($send->getSendCheck()) {
+		$cookie = new Policy;
+		$cookie->setCookieIfMessageSent();
+	}
+
 } catch (Exception $e) {
 	$alert = new Alert('danger', $e->getMessage());
 	echo $alert->getAlert();
